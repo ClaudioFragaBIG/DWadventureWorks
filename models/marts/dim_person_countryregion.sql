c3ef3d3033f089efe4979cba0a3061086bd695e2
claudio.fraga@burgerking.com.br <claudio.fraga@burgerking.com.br>
2021-11-07 22:38:24 +0000

    SecondBigCommit

--- models/marts/dim_person_countryregion.sql
+++ models/marts/dim_person_countryregion.sql
@@ -4,7 +4,7 @@ with
     staging as (
         select *
         from {{ref('stg_person_countryregion')}}
-    )
+    ) -- referenciando a country region
     , transformed as (
         select
             row_number() over (order by countryregioncode) as countryregioncode_sk -- auto-incremental surrogate key
